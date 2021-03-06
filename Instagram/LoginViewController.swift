32414124276b376c5ded3c12214bdd752072da92
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-03-06 13:59:02 +0900

    ログイン画面を完成させる１完了

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -6,9 +6,61 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var mailAdressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    //ログインボタンをタップした時に呼ばれるメソッド
+    @IBAction func handleLoginButton(_ sender: Any) {
+    }
+    
+    //アカウント作成ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAdressTextField.text, let passwaord = passwordTextField.text, let displayName = displayNameTextField.text {
+            //アドレス、パスワード、表示名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || passwaord.isEmpty || displayName.isEmpty {
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+            
+            //アドレスとパスワードでユーザー作成。ユーザー作成に成功すると自動的にログインする
+            Auth.auth().createUser(withEmail: address, password: passwaord){ authResult, error in
+                if let error = error {
+                    //エラーがあったら原因をprintしてreturnすることで以降の処理を実行せず終了する
+                    print("DEBUG_PRINT:" + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザーの作成に成功しました。")
+                
+                //表示名を設定する
+                //Auth.auth().currentUserで現在サインインしているユーザーを取得、ユーザーがサインインしていない場合はnilを返す
+                let user = Auth.auth().currentUser
+                //userがnilでなかったら
+                if let user = user {
+                    //createProfileChangeRequest()でユーザーのプロファイルを更新する
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    //commitChangeで更新する
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            //プロフィールの更新でエラーが発生
+                            print("DEBUG_PRINT:" + error.localizedDescription)
+                            return
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)] の設定に成功しました。")
+                        
+                        //画面を閉じてタブ画面に戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
