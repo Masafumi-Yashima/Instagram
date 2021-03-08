3c0f22c903a32ddda70fffe725be94c552cf53ed
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-03-09 00:53:42 +0900

    表示するときにUIImageViewへ画像を設定する完了

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -6,20 +6,28 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class PostViewController: UIViewController {
+    var image:UIImage!
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
     
+    //投稿ボタンをタップした時に呼ばれるメソッド
     @IBAction func handlePostButton(_ sender: Any) {
     }
     
+    //キャンセルボタンをタップした時に呼ばれるメソッド
     @IBAction func handleCancelButton(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //受け取った画像をImageViewに設定する
+        imageView.image = image
         // Do any additional setup after loading the view.
     }
     
