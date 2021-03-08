3475f3157084c1ac21851ececa8a980921764c97
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-03-09 01:15:28 +0900

    投稿する完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by YashimaMasafumi on 2021/03/09.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
